646a802cb5712ae03d3892a23134f6d15594ae24
b22060 <e1b22060@oit.ac.jp>
2024-10-22 16:49:09 +0900

    matchinfoテーブルをDBに追加

--- src/main/java/oit/is/z2535/kaizi/janken/model/Match.java
+++ src/main/java/oit/is/z2535/kaizi/janken/model/Match.java
@@ -7,41 +7,61 @@ public class Match {
   String user1Hand;
   String user2Hand;
   String result;
+  Boolean isActive;
 
   public int getId() {
     return id;
   }
+
   public void setId(int id) {
     this.id = id;
   }
+
   public int getUser1() {
     return user1;
   }
+
   public void setUser1(int user1) {
     this.user1 = user1;
   }
+
   public int getUser2() {
     return user2;
   }
+
   public void setUser2(int user2) {
     this.user2 = user2;
   }
+
   public String getUser1Hand() {
     return user1Hand;
   }
+
   public void setUser1Hand(String user1Hand) {
     this.user1Hand = user1Hand;
   }
+
   public String getUser2Hand() {
     return user2Hand;
   }
+
   public void setUser2Hand(String user2Hand) {
     this.user2Hand = user2Hand;
   }
+
   public String getResult() {
     return result;
   }
+
   public void setResult(String result) {
     this.result = result;
+  }
+
+  public Boolean getIsActive() {
+    return isActive;
+  }
+
+  public void setIsActive(Boolean isActive) {
+    this.isActive = isActive;
   }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO users (userName) VALUES ('CPU');
 INSERT INTO users (userName) VALUES ('ほんだ');
-INSERT INTO matches (user1, user2, user1Hand, user2Hand, result) VALUES (2, 1, 'Gu', 'Choki', '勝ち');
-INSERT INTO matches (user1, user2, user1Hand, user2Hand, result) VALUES (2, 1, 'Gu', 'Gu', 'あいこ');
-INSERT INTO matches (user1, user2, user1Hand, user2Hand, result) VALUES (2, 1, 'Gu', 'Pa', '負け');
+INSERT INTO users (userName) VALUES ('いがき');
+INSERT INTO matches (user1, user2, user1Hand, user2Hand,result,isActive) VALUES (2, 1, 'Gu', 'Choki', '勝ち','FALSE');
+INSERT INTO matches (user1, user2, user1Hand, user2Hand,result,isActive) VALUES (2, 1, 'Gu', 'Gu', 'あいこ','FALSE');
+INSERT INTO matches (user1, user2, user1Hand, user2Hand,result,isActive) VALUES (2, 1, 'Gu', 'Pa', '負け','FALSE');
+INSERT INTO matchinfo (user1, user2, user1Hand, isActive) VALUES (1, 2, 'Gu', 'FALSE');
+INSERT INTO matchinfo (user1, user2, user1Hand, isActive) VALUES (2, 3, 'Gu', 'FALSE');
